2050901049c64b1b3410218dd2524870cc84ea93
randauto <tuanlqhvu@gmail.com>
2014-06-07 16:37:06 +0700

    add introduction

--- source code/LuatHangHai/src/com/vinilearning/maritimelaw/activity/IntroductionActivity.java
+++ source code/LuatHangHai/src/com/vinilearning/maritimelaw/activity/IntroductionActivity.java
@@ -2,11 +2,13 @@ package com.vinilearning.maritimelaw.activity;
 
 import android.os.Bundle;
 
+import com.vinilearning.maritimelaw.R;
 import com.vinilearning.maritimelaw.common.BaseActivity;
 
 public class IntroductionActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.introduction_layout);
 	}
 }
